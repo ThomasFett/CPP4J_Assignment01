dbbff4e86fca798c254023be7d2c441d445178ce
Thomas Fett <thomasfett@gmx.net>
2012-10-13 15:05:34 +0200

    added all function stubs and checked that the test program runs (although the tests fail of course)

--- rationalnumber.cpp
+++ rationalnumber.cpp
@@ -1,5 +1,38 @@
 #include "rationalnumber.h"
 
-RationalNumber::RationalNumber()
+bool rnIsValid(RationalNumber n)
 {
+    return false;
+}
+
+bool rnEqual(RationalNumber n1, RationalNumber n2)
+{
+    return false;
+}
+
+bool rnLessThan(RationalNumber n1, RationalNumber n2)
+{
+    return false;
+}
+
+RationalNumber rnAdd(RationalNumber n1, RationalNumber n2)
+{
+    RationalNumber result = {1,1};
+    return result;
+}
+
+RationalNumber rnSubtract(RationalNumber n1, RationalNumber n2)
+{
+    RationalNumber result = {1,1};
+    return result;}
+
+RationalNumber rnMultiply(RationalNumber n1, RationalNumber n2)
+{
+    RationalNumber result = {1,1};
+    return result;}
+
+RationalNumber rnDivide(RationalNumber n1, RationalNumber n2)
+{
+    RationalNumber result = {1,1};
+    return result;
 }
--- rationalnumber.h
+++ rationalnumber.h
@@ -1,10 +1,21 @@
 #ifndef RATIONALNUMBER_H
 #define RATIONALNUMBER_H
 
-class RationalNumber
-{
-public:
-    RationalNumber();
+// the structr that represents a rational number (German: "numerator" is "Zaehler" & "denominator" is "Nenner").
+struct RationalNumber {
+    int numerator;
+    int denominator;
 };
+
+/* Declarations of all the functions that are used in 'testRN.cpp'. The function that normalizes a RationalNumber is not public yet and only
+used in the implementation file('rationalnumber.cpp'). */
+
+bool rnIsValid(RationalNumber n);
+bool rnEqual(RationalNumber n1, RationalNumber n2);
+bool rnLessThan(RationalNumber n1, RationalNumber n2);
+RationalNumber rnAdd(RationalNumber n1, RationalNumber n2);
+RationalNumber rnSubtract(RationalNumber n1, RationalNumber n2);
+RationalNumber rnMultiply(RationalNumber n1, RationalNumber n2);
+RationalNumber rnDivide(RationalNumber n1, RationalNumber n2);
 
 #endif // RATIONALNUMBER_H
