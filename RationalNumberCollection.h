1fb9d8e24747cb85de7d5ac63fcb5e65699bae2b
Thomas Fett <thomasfett@gmx.net>
2012-10-13 23:34:55 +0200

    completed the public documentation for RationalNumberCollection in the header file

--- RationalNumberCollection.h
+++ RationalNumberCollection.h
@@ -3,26 +3,40 @@
 
 #include <RationalNumber.h>;
 
+/*A RationalNumberCollection can contain up to 1000 different RationalNumbers
+  and contains the count of each kind of RationalNumber*/
 struct RationalNumberCollection {
     int collection[1500];
 };
 
+// inits the created RationalNumberCollection by filling it with zeros.
+// CAUTION! Don't call this method, wenn the RNC is already in use!
 void rncInit(RationalNumberCollection* c);
 
+// adds a given RationalNumber to the given RationalNumberCollection
 void rncAdd(RationalNumberCollection* c, RationalNumber n);
 
+// Returns the count of the given RationalNumber. If the RationalNumber is not in the RNC the return value is 0.
 int rncCount(RationalNumberCollection* c, RationalNumber n);
 
+// removes a given RationalNumber from the given RationalNumberCollection
+// returns TRUE if all RationalNumbers of this kind are removed afterwards
+// returns FALSE if there are still RationalNumbers of that kind are left after the removal
 bool rncRemove(RationalNumberCollection* c, RationalNumber n);
 
+// returns the count of DIFFERENT RationalNUmbers in the collection
 int rncTotalUniqueCount(RationalNumberCollection* c);
 
+// returns the count of ALL RationalNumbers in the collection
 int rncTotalCount(RationalNumberCollection* c);
 
+// sums up ALL RationalNumbers in the collection
 RationalNumber rncSum(RationalNumberCollection* c);
 
+// calculates the average Value of ALL RationalNumbers in the collection
 RationalNumber rncAverage(RationalNumberCollection* c);
 
+// calculates the median value of ALL RationalNumbers in the collection
 RationalNumber rncMedian(RationalNumberCollection* c);
 
 #endif // RATIONALNUMBERCOLLECTION_H
